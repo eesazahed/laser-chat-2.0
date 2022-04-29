e467aa4afeec8720eccb85d19c3e1ff898d58228
Eesa Zahed <97064249+EesaZahed@users.noreply.github.com>
2022-04-28 23:59:31 -0400

    fixed scrollbar css

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -8,6 +8,13 @@ export const SERVERNAME = "Laser Chat v2.0.1";
 
 const firebaseConfig = {
   // Hello. Please look over the README for full instructions for setting up a chat server.
+  apiKey: "AIzaSyDlROHrhsayH2xRbWtCqHbnL4jostuCdzg",
+  authDomain: "project-laser-36c53.firebaseapp.com",
+  databaseURL: "https://project-laser-36c53-default-rtdb.firebaseio.com",
+  projectId: "project-laser-36c53",
+  storageBucket: "project-laser-36c53.appspot.com",
+  messagingSenderId: "950500432563",
+  appId: "1:950500432563:web:5258c1c13516aba0616ec6",
 };
 
 const app = initializeApp(firebaseConfig);
